2d7c1c97f2a2150e498f7cf28d794c910ada7475
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2023-02-28 15:03:47 -0500

    Add pokegame.js

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,23 +1,12 @@
 import { Component } from "react";
 import Pokecard from "./Pokecard";
+import "./App.css";
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ],
-  };
+
   render() {
     return (
       <div className="pokedex">
-        <h1>Pokedex!</h1>
         {this.props.pokemon.map((p) => (
           <div key={p.id}>
             <Pokecard
@@ -32,6 +21,5 @@ class Pokedex extends Component {
     );
   }
 }
-
 
 export default Pokedex;
