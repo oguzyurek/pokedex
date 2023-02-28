f2937643fa38a0d541a2a4a7ad4c142afe413580
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2023-02-28 15:17:28 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
+import Pokegame from "./Pokegame";
 import "./index.css";
-import Pokedex from "./Pokedex";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
-      
+        <Pokegame />
       </div>
     );
   }
