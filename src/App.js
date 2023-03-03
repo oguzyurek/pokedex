bcf246efc213bbfe21437a5d56e96d193f49c549
oguzyurek <105684710+oguzyurek@users.noreply.github.com>
2023-03-03 00:26:18 -0500

    Add state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Pokegame from "./Pokegame";
+import Game from "./Game";
 import "./index.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Pokegame />
+        <Game />
       </div>
     );
   }
